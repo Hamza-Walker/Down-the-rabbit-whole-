ebdd2e3a3f7f73e94e1344659ce3b56bdbbde32e
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-07-09 08:57:23 +0200

    set up complete

---
+++ src/main/java/com/walker/Application.java
@@ -0,0 +1,21 @@
+package com.walker;
+
+public class Application {
+
+    private static final String WORK_DIR = "src/main/resources/";
+
+    public static void main(String[] args)
+    {
+        splitRepublic();
+        splitAlice();
+
+    }
+
+    private static void splitRepublic()
+    {
+    }
+
+    private static void splitAlice()
+    {
+    }
+}
