f5bc937c9c77bd4d34bb16506160bf0a6fa4422d
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-07-09 20:01:39 +0200

    practice tasks completed and managed to split alice in wonderland as well, i used streams where it was reasonable and avoided creating unreadable code, overall the tasks were fun and i enjoyed them

--- src/main/java/com/walker/Application.java
+++ src/main/java/com/walker/Application.java
@@ -8,15 +8,19 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
 
     private static final String WORK_DIR = "src/main/resources/";
     private static final String SOURCE_FILE = WORK_DIR + "the-republic.txt";
+    private static final String SOURCE_FILE2= WORK_DIR + "alice-in-wonderland.txt";
     private static final String OUTPUT_FOLDER = WORK_DIR + "the-republic/";
+    private static final String OUTPUT_FOLDER2 = WORK_DIR + "Alice-in-wonderland/";
 
     public static void main(String[] args) {
         splitRepublic();
+        splitAliceInWonderland();
     }
 
     private static void splitRepublic() {
@@ -74,4 +78,59 @@ public class Application {
         return chapterIndices;
     }
 
+
+    private static void splitAliceInWonderland() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(SOURCE_FILE2))) {
+            List<String> lines = reader.lines().collect(Collectors.toList());
+
+            // Create the output folder
+            File folder = new File(OUTPUT_FOLDER2);
+            if (!folder.exists()) {
+                folder.mkdirs();
+            }
+
+            // Find the titles and indices
+            List<String> chapterTitles = findChapterTitles(lines);
+            List<Integer> chapterIndices = findChapterIndicesAlice(lines, chapterTitles);
+
+            // Split the text into chapters and write to separate files
+            for (int i = 0; i < chapterIndices.size(); i++) {
+                int startIndex = chapterIndices.get(i);
+                int endIndex = (i + 1 < chapterIndices.size()) ? chapterIndices.get(i + 1) : lines.size();
+                List<String> chapterLines = lines.subList(startIndex, endIndex);
+
+                // Extract the chapter name from the chapter title line
+                String chapterTitle = chapterTitles.get(i);
+
+                // Write the chapter text to a file
+                String filename = OUTPUT_FOLDER2 + "alice-in-wonderland - " + chapterTitle + ".txt";
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+                    for (String chapterLine : chapterLines) {
+                        writer.write(chapterLine);
+                        writer.newLine();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("Splitting completed. Output files are located in: " + OUTPUT_FOLDER2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static List<String> findChapterTitles(List<String> lines) {
+        return lines.stream()
+                .filter(line -> line.startsWith("CHAPTER"))
+                .map(line -> line.trim())
+                .collect(Collectors.toList());
+    }
+
+    private static List<Integer> findChapterIndicesAlice(List<String> lines, List<String> chapterTitles) {
+      return chapterTitles.stream()
+              .map(title -> lines.indexOf(title))
+              .filter(index -> index !=-1)
+              .collect(Collectors.toList());
+    }
 }
